7a2ed33e61c87c43477813fcf521aee0a2b7fab2
Muhammad Syauqi Amiq Amrullah <syauqiamiq12@gmail.com>
2022-04-18 13:05:11 +0700

    feat: First Init

---
+++ components/template/Template.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Template = ({children}) => {
+  return (
+      
+  );
+};
+
+export default Template;
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Landing Page</title>
+        <meta name="description" content="Landing Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}

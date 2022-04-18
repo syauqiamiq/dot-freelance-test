0b7256b4d1b9767336b5c4c37fa115938e0915e0
Muhammad Syauqi Amiq Amrullah <syauqiamiq12@gmail.com>
2022-04-19 05:25:21 +0700

    fix: Navbar Responsive and Add Form Auth Component

--- components/AuthFormComponent/AuthFormComponent.jsx
+++ components/AuthFormComponent/AuthFormComponent.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import { Alert, Button, Card, Form } from 'react-bootstrap';
+import styles from './form.module.css';
+const AuthFormComponent = ({ isRegisterForm }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
 
-const AuthFormComponent = () => {
-  return <div>AuthFormComponent</div>;
+  const router = useRouter();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email == 'admin@admin.com' && password == 'admin') {
+      localStorage.setItem('isAuth', true);
+      router.push('/');
+    } else {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 5000);
+    }
+  };
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label className={styles.formLabel}>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label className={styles.formLabel}>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </Form.Group>
+
+        {error ? (
+          <Form.Group className="mb-3">
+            <Alert variant="danger">Username / Password Salah</Alert>
+          </Form.Group>
+        ) : (
+          ''
+        )}
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default AuthFormComponent;
--- components/NavbarComponent/NavbarComponent.jsx
+++ components/NavbarComponent/NavbarComponent.jsx
@@ -4,23 +4,25 @@ import styles from './navbar.module.css';
 
 const NavbarComponent = () => {
   return (
-    <Navbar className={styles.navbar} expand="lg">
-      <Container>
-        <Navbar.Brand className={styles.navbarBrand}>CryptoKu</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="d-flex justify-content-end" id="basic-navbar-nav">
-          <Nav>
-            <Link href="/">
-              <a className={styles.navLink}> Home</a>
-            </Link>
-            <Link href="/login">
-              <a className={styles.navLink}>Sign In</a>
-            </Link>
-            <Button className={styles.btnLogin}>Sign Up</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar collapseOnSelect className={styles.navbar} expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand className={styles.navbarBrand}>CryptoKu</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse className="d-flex justify-content-end" id="responsive-navbar-nav">
+            <Nav>
+              <Link href="/">
+                <a className={styles.navLink}> Home</a>
+              </Link>
+              <Link href="/login">
+                <a className={styles.navLink}>Sign In</a>
+              </Link>
+              <Button className={styles.btnLogin}>Sign Up</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 

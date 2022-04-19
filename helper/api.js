c0adda5caf893082ef41b73f58cd5f55391e6d9b
Muhammad Syauqi Amiq Amrullah <syauqiamiq12@gmail.com>
2022-04-19 07:59:45 +0700

    fix: Some Issue

--- helper/api.js
+++ helper/api.js
@@ -1,8 +1,2 @@
 // FROM COINGECKO WEBSITE
 export const GET_CRYPTO_CATEGORY_URL = 'https://api.coingecko.com/api/v3/coins/categories';
-
-// FROM API ROUTE
-export const BASE_URL = 'http://localhost:3000';
-export const LOCAL_GET_CITY_URL = BASE_URL + '/api/rajaongkir/city';
-export const LOCAL_GET_PROVINCE_URL = BASE_URL + '/api/rajaongkir/province';
-export const LOCAL_POST_COST_URL = BASE_URL + '/api/rajaongkir/cost';
